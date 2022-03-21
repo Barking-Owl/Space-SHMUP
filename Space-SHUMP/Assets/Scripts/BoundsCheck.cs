cf5843d05556c084f78fbb04cf0fc3463fa3d45b
A.N <77659600+Barking-Owl@users.noreply.github.com>
2022-03-21 15:09:23 -0500

    Bounding box implementation

--- Space-SHUMP/Assets/Scripts/BoundsCheck.cs
+++ Space-SHUMP/Assets/Scripts/BoundsCheck.cs
@@ -87,7 +87,13 @@ public class BoundsCheck : MonoBehaviour
     }//end LateUpdate
 
     //Draw the bounds in the scene pane
-
+    private void OnDrawGizmos()
+    {
+        if (!Application.isPlaying) return;
+        Vector3 boundSize = new Vector3(camWidth * 2, camHeight * 2, 0.1f);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(Vector3.zero, boundSize);
+    } //end OnDrawGizmos()
 
 
 
